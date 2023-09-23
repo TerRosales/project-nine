25a12b093dbbcf9acc94573b2dcdde6836673072
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-09-23 13:55:08 -0400

    slide to the right - slide to the left

--- js/app.js
+++ js/app.js
@@ -8,3 +8,27 @@ function toggleMode() {
 }
 
 modeToggle.addEventListener('click', toggleMode);
+
+// slide in
+
+const items = document.querySelectorAll('.item:not(:first-child)');
+
+const options = {
+    threshold: 0.6
+}
+
+function addSlideIn(entries) {
+    entries.forEach(entry => {
+if (entry.isIntersecting) {
+    entry.target.classList.add('slide-in');
+} else {
+    entry.target.classList.remove ('slide-in')
+}
+});
+}
+
+const observer = new IntersectionObserver(addSlideIn, options);
+
+items.forEach(item => {
+    observer.observe(item);
+})
